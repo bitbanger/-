6b8e459f1619b93d6d297663accfa0ccfe2f9561
Lane <lanelawley@gmail.com>
2026-02-13 21:12:06 -0500

    x

--- t.py
+++ t.py
@@ -3,17 +3,21 @@ import sys
 
 
 def parse_set(s):
+	grade = ''
+	if len(graspl:=s.split('*')) == 2:
+		s, grade = s.split('*')
+
 	var = ''
 	if '[' in s:
 		var = ll.regf('\[(.*)\]')(s)
 
 	fset = s.split('[')[0].strip().replace('&', '').replace(' ', '_').lower()
 	
-	return fset, var
+	return fset, var, grade
 
 
 def get_cards(sport, set, var, quants_by_num, whole=True):
-	fns = [fn for fn in ll.ls(f'scp_csvs') if fn.endswith(f'{set}.csv') and ll.bn(fn).startswith(f'{sport}_')]
+	fns = [fn for fn in ll.ls(f'scp_csvs') if fn.endswith(f'{set.split("*")[0]}.csv') and ll.bn(fn).startswith(f'{sport}_')]
 	if len(fns) == 0:
 		ll.err(f"no csv file found for [grey70]{sport}[/grey70] set [grey70]{set}[/grey70]")
 	assert(len(fns)==1)
@@ -76,6 +80,10 @@ def agg_quants_by_num_by_set(fn):
 	return quants_by_num_by_set
 
 
+def is_psa_10(grade):
+	return grade.strip() == 'PSA 10'
+
+
 def process(fn, console=True):
 	fn_dir = ll.dn(fn)
 	fn = ll.bn(fn)
@@ -94,13 +102,18 @@ def process(fn, console=True):
 			gotten = 0
 
 			for set, quants_by_num in quants_by_num_by_set.items():
+				grade = ''
+				if len(graspl:=set.split('*')) == 2:
+					set, grade = set.split('*')
+
 				# Get card info
-				cur_fset, var = parse_set(set)
+				cur_fset, var, _ = parse_set(set)
 				# card_row = get_card(sport, cur_fset, var, num)
 				card_rows = get_cards(sport, cur_fset, var, quants_by_num)
 
 				for card_row in card_rows:
-					price = float(card_row['loose-price'][1:] or 0)
+					price_key = 'manual-only-price' if is_psa_10(grade) else 'loose-price'
+					price = float(card_row[price_key][1:] or 0)
 
 					# Console output
 					name = card_row['product-name'].split('#')[0].split(' [')[0].strip()
@@ -112,36 +125,32 @@ def process(fn, console=True):
 					unvar_set = set.split(' [')[0].strip()
 					year = unvar_set.split(' ')[0].strip()
 					unyear_set = ' '.join(unvar_set.split(' ')[1:]).strip()
-					'''
-					if console:
-						if price >= 4.00:
-							print(f'[yellow3]{price:.02f}[/yellow3]\t{name} {set}')
-						elif price >= 1:
-							print(f'[grey50]{price:.02f}[/grey50]\t{name} {set}')
-						elif price == 0:
-							print(f'[grey50]-[/grey50]\t{name} {set}')
-						else:
-							print(f'[grey30]{price:.02f}[/grey30]\t{name} {set}')
-					'''
 
 					# File output
-					card_tup = (sport, year, unyear_set, name, num, var, price)
+					card_tup = (sport, year, unyear_set, name, num, var, price, grade)
 					yield card_tup
 
 					if var:
-						card_f.write(f'{name} #{num} [{var}] {unvar_set}\n')
-						price_f.write(f'{price:.02f}\t{name} #{num} [{var}] {unvar_set}\n')
+						card_str = f'{name} #{num} [{var}] {unvar_set}'
 					else:
-						card_f.write(f'{name} #{num} {unvar_set}\n')
-						price_f.write(f'{price:.02f}\t{name} #{num} {unvar_set}\n')
+						card_str = f'{name} #{num} {unvar_set}'
+					if grade:
+						card_str += f' ({grade})'
+
+					card_f.write(f'{card_str}\n')
+					price_f.write(f'{price:.02f}\t{card_str}\n')
+
 					gotten += 1
 
 
-def print_card(sport, year, set, name, num, var, price):
+def print_card(sport, year, set, name, num, var, price, grade):
 	if var:
 		card_str = f'{name} #{num} [{var}] {year} {set}'
 	else:
 		card_str = f'{name} #{num} {year} {set}'
+
+	if grade:
+		card_str += f' ({grade})'
 
 	if price >= 4.00:
 		print(f'[yellow3]{price:.02f}[/yellow3]\t{card_str}')
@@ -195,16 +204,16 @@ def main():
 
 	def _track_it():
 		for fn in fns:
-			for price in process(fn):
-				yield price
+			for card in process(fn):
+				yield card
 	
 	got = 0
 	good_got = 0
 	value = 0
 	good_value = 0
 	thresh = 4.00
-	for (sport, year, set, name, num, var, price) in ll.track(_track_it(), total=total):
-		print_card(sport, year, set, name, num, var, price)
+	for (sport, year, set, name, num, var, price, grade) in ll.track(_track_it(), total=total):
+		print_card(sport, year, set, name, num, var, price, grade)
 
 		got += 1
 		value += price
