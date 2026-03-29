3a6cdf5ab9bd334415c1f5ef6daabc0ea97aa0fc
Lane <lanelawley@gmail.com>
2026-03-29 17:18:19 -0400

    x

--- stsearch.py
+++ stsearch.py
@@ -11,6 +11,8 @@ def main():
 	ap = ArgumentParser()
 	ap.add_argument('set', nargs='+')
 	ap.add_argument('-d', '--dir', default='new_scp_csvs')
+	ap.add_argument('-m', '--minimal', action='store_true')
+	ap.add_argument('-p', '--parallels', action='store_true')
 	args = ap.parse_args()
 
 	args.set = ' '.join(args.set)
@@ -24,18 +26,26 @@ def main():
 	top = [s for s, sc in matches.items() if sc==max(matches.values())]
 	year = lambda s: int(ll.regf('[0-9]'*4)(s) or '0')
 	top = [x for x in top if year(x[1])==max([year(s[1]) for s in top])]
-	best_fn, best_set = min(sorted(top, key=ll.nth(1)), key=lambda e: len(e[1]))
+	top = sorted(top, key=ll.nth(1))
+	best_fn, best_set = min(top, key=lambda e: len(e[1]))
 
-	parallels = set()
-	for row in ll.csv(best_fn):
-		if (par:=ll.regf('\\[(.*)\\]')(row['product-name'])):
-			parallels.add(par)
-	parallels = sorted(list(parallels))
+	if args.minimal:
+		top = [(best_fn, best_set)]
 
 	print('')
-	print(best_set)
-	for p in parallels:
-		print(f'\t[{p}]')
+	for c_fn, c_set in top:
+		print(c_set)
+
+		if args.parallels:
+			parallels = set()
+			for row in ll.csv(c_fn):
+				if (par:=ll.regf('\\[(.*)\\]')(row['product-name'])):
+					parallels.add(par)
+			parallels = sorted(list(parallels))
+
+			for p in parallels:
+				print(f'\t[{p}]')
+
 	print('')
 
 	pass
